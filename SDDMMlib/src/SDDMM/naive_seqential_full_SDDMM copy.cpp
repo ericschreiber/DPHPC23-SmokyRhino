719d905995b74899f03928c95e486be259531e95
Jiela42 <knechtd@student.ethz.ch>
2023-10-11 15:37:04 +0200

    added two naive implementations of the SDDMM, ran into templating problems and did not (yet) make tests or add to the readme.

---
+++ SDDMMlib/src/SDDMM/naive_seqential_full_SDDMM copy.cpp
@@ -0,0 +1,42 @@
+// naive_SDDMM.cpp
+#include "naive_SDDMM.hpp"
+#include <iostream>
+
+template <typename T>
+void naive_sequential_full_SDDMM<T>::SDDMM(const DenseMatrix<T>& x, const DenseMatrix<T>& y, const SparseMatrix<T>& z, SparseMatrix<T>& result) const {
+
+    // This is a very dumb implementation, because it samples only AFTER the matrix x matrix multiplication
+
+    int m = x.getNumRows();
+    int n = x.getNumCols();
+    int k = y.getNumCols();
+
+    DenseMatrix xy = DenseMatrix(m, n); 
+
+    // I assume a size check has been done for now, but we might want to make that one explicitly
+    // Please note that the paper uses A[M][K] and B[N][K]. I.e. B already seems to be transposed!
+        // I am making the same assumption
+    // I also assume we are taking a CRS matrix
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            for(int l = 0; l < k; l++){
+                xy[i][j] += x[i][l] * y[j][l];
+            }
+        }
+    }
+
+    for(int i = 0; i < m; i ++){
+        for(int j = z.getRowPtr[i]; j < z.getRowPtr[i+1]; j++){
+            result.getValues[j] *= z.getValues[j];
+        }
+    }
+
+    std::cout << "naive_sequential_sampled_SDDMM was executed :)" << std::endl;
+    return;
+}
+
+// Explicit template instantiation
+template class naive_SDDMM<float>;
+template class naive_SDDMM<double>;
+template class naive_SDDMM<int>;
---
+++ SDDMMlib/src/SDDMM/naive_seqential_sampled_SDDMM.cpp
@@ -0,0 +1,40 @@
+// naive_SDDMM.cpp
+#include "naive_SDDMM.hpp"
+#include <iostream>
+
+template <typename T>
+void naive_sequential_sampled_SDDMM<T>::SDDMM(const DenseMatrix<T>& x, const DenseMatrix<T>& y, const SparseMatrix<T>& z, SparseMatrix<T>& result) const {
+
+    // This is literally just a straightforward implementation of Algorithm 2 in the SDMM HPC Paper
+
+    int m = x.getNumRows();
+    int n = x.getNumCols();
+    int k = y.getNumCols();
+
+    // I assume a size check has been done for now, but we might want to make that one explicitly
+    // Please note that the paper uses A[M][K] and B[N][K]. I.e. B already seems to be transposed!
+        // I am making the same assumption
+    // I also assume we are taking a CRS matrix
+
+    for(int i = 0; i < m; i++){
+        for(int j = z.getRowPtr[i]; j < z.getRowPtr[i+1]; j++){
+            for(int l = 0; l < k; l++){
+                result.getValues[j] += x[i][l] * y[z.getColIndices[j]][l];
+            }
+        }
+    }
+
+    for(int i = 0; i < m; i ++){
+        for(int j = z.getRowPtr[i]; j < z.getRowPtr[i+1]; j++){
+            result.getValues[j] *= z.getValues[j];
+        }
+    }
+
+    std::cout << "naive_sequential_sampled_SDDMM was executed :)" << std::endl;
+    return;
+}
+
+// Explicit template instantiation
+template class naive_SDDMM<float>;
+template class naive_SDDMM<double>;
+template class naive_SDDMM<int>;

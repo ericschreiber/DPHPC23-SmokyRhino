4947295d73bcc06c22175962164c105fa33456de
Niklas Roemer <nroemer@ethz.ch>
2023-10-13 13:07:27 +0200

    add comments + renaming

--- initial_test/old_files/compile.sh
+++ initial_test/old_files/compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 rm -f *.o *.exe
-nvcc -c naive_dense_dense.cu -o naive_dense_dense.o -I../../common/inc -I../include -O3 -std=c++11 \
+nvcc -c main.cu -o main.o -I../../common/inc -I../include -O3 -std=c++11 \
                     --compiler-options "-Wall -Wextra" -use_fast_math -lcublas \
                     -gencode=arch=compute_86,code=sm_86 -lineinfo
-nvcc -c my_naive_sampling.cu -o my_naive_sampling.o -I../../common/inc -I../include -O3 -std=c++11 \
+nvcc -c sampling.cu -o sampling.o -I../../common/inc -I../include -O3 -std=c++11 \
                     --compiler-options "-Wall -Wextra" -use_fast_math -lcublas \
                     -gencode=arch=compute_86,code=sm_86 -lineinfo
-nvcc naive_dense_dense.o my_naive_sampling.o -o execute.exe -O3 -std=c++11 \
+nvcc main.o sampling.o -o execute.exe -O3 -std=c++11 \
                     --compiler-options "-Wall -Wextra" -use_fast_math -lcublas \
                     -gencode=arch=compute_86,code=sm_86 -lineinfo

747643f118e8bed01291df5095976627386e6b86
Niklas Roemer <nroemer@ethz.ch>
2023-10-16 14:02:49 +0200

    additional coments for initial_SDDMM_GPU_niklas

--- SDDMMlib/src/SDDMM/niklas_implementation/initial_SDDMM_GPU_niklas.cpp
+++ SDDMMlib/src/SDDMM/niklas_implementation/initial_SDDMM_GPU_niklas.cpp
@@ -3,9 +3,18 @@
 #include <iostream>
 
 template <typename T>
-void naive_SDDMM<T>::SDDMM(const DenseMatrix<T>& matrixA, const DenseMatrix<T>& matrixB, const SparseMatrix<T>& matrixC, SparseMatrix<T>& matrixResult) const {
+void naive_SDDMM<T>::SDDMM(const DenseMatrix<T>& matrixA,
+                           const DenseMatrix<T>& matrixB,
+                           const SparseMatrix<T>& matrixC,
+                           SparseMatrix<T>& matrixResult) const
+{
     /*
     // get values colIndices and rowPtr from matrixC
+    std::vector<T> values_matrixC = matrixC.getValues();
+    std::vector<int> colIndices_matrixC = matrixC.getColIndices();
+    std::vector<int> rowPtr_matrixC = matrixC.getRowPtr();
+
+
     */
     std::cout << "naive_SDDMM from niklas was executed :)" << std::endl;
     return;

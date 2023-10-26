5abf3a237519529d77f6b407ec3e1f9781302d1e
Eric <eric.schreiber@gmx.ch>
2023-10-26 17:28:30 +0200

    Allowing implementations only for one type to be possible.

--- SDDMMlib/src/SDDMM/naive_SDDMM.cpp
+++ SDDMMlib/src/SDDMM/naive_SDDMM.cpp
@@ -3,19 +3,19 @@
 
 #include <iostream>
 
-template <typename T>
-void naive_SDDMM<T>::SDDMM(
-    const DenseMatrix<T>& x,
-    const DenseMatrix<T>& y,
-    const SparseMatrix<T>& z,
-    SparseMatrix<T>& result) const
+// Only the float type of the class is valid all other types will throw an error
+void naive_SDDMM<float>::SDDMM(
+    const DenseMatrix<float>& x,
+    const DenseMatrix<float>& y,
+    const SparseMatrix<float>& z,
+    SparseMatrix<float>& result) const
 {
     // Check if CSRMatrix
-    const CSRMatrix<T>* csrMatrix = dynamic_cast<const CSRMatrix<T>*>(&z);
-    CSRMatrix<T>* csrResult = dynamic_cast<CSRMatrix<T>*>(&result);
+    const CSRMatrix<float>* csrMatrix = dynamic_cast<const CSRMatrix<float>*>(&z);
+    CSRMatrix<float>* csrResult = dynamic_cast<CSRMatrix<float>*>(&result);
     if (csrMatrix == nullptr || csrResult == nullptr)
     {
-        throw std::invalid_argument("Error: naive_SDDMM::SDDMM() only accepts CSRMatrix<T> as input. Other formats are not supported yet");
+        throw std::invalid_argument("Error: naive_SDDMM::SDDMM() only accepts CSRMatrix<float> as input. Other formats are not supported yet");
     }
     else
     {
@@ -26,11 +26,20 @@ void naive_SDDMM<T>::SDDMM(
 }
 
 template <typename T>
-void naive_SDDMM<T>::naive_SDDMM_CSR(
+void naive_SDDMM<T>::SDDMM(
     const DenseMatrix<T>& x,
     const DenseMatrix<T>& y,
-    const CSRMatrix<T>& z,
-    CSRMatrix<T>& result) const
+    const SparseMatrix<T>& z,
+    SparseMatrix<T>& result) const
+{
+    assert(false && "Error: naive_SDDMM::SDDMM() only accepts float as input. Other types are not supported yet");
+}
+
+void naive_SDDMM<float>::naive_SDDMM_CSR(
+    const DenseMatrix<float>& x,
+    const DenseMatrix<float>& y,
+    const CSRMatrix<float>& z,
+    CSRMatrix<float>& result) const
 {
     // please implement
     std::cout << "naive_SDDMM was executed :)" << std::endl;
@@ -38,6 +47,6 @@ void naive_SDDMM<T>::naive_SDDMM_CSR(
 }
 
 // Explicit template instantiation
-template class naive_SDDMM<float>;
+// template class naive_SDDMM<float>;
 template class naive_SDDMM<double>;
 template class naive_SDDMM<int>;

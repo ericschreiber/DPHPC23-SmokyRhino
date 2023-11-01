4ab8236ec9dac334bd9be6ebb95b5222354ae345
Jiela42 <knechtd@student.ethz.ch>
2023-11-01 15:06:30 +0100

    commit to merge

--- SDDMMlib/include/SDDMM/naive_sequential_full_SDDMM_HOST/naive_sequential_full_SDDMM_HOST.hpp
+++ SDDMMlib/include/SDDMM/naive_sequential_full_SDDMM_HOST/naive_sequential_full_SDDMM_HOST.hpp
@@ -10,6 +10,9 @@ template <typename T>
 class naive_sequential_full_SDDMM_HOST : public SDDMMlib<T>
 {
     public:
+        naive_sequential_full_SDDMM_HOST() { assert(false && "Error: naive_sequential_full_SDDMM_HOST<T>::naive_SDDMM() is only implemented for float."); }
+        naive_sequential_full_SDDMM_HOST(ExecutionTimer* timer) { assert(false && "Error: naive_sequential_full_SDDMM_HOST<T>::naive_SDDMM() is only implemented for float."); }
+        
         virtual void SDDMM(
             const DenseMatrix<T>& x,
             const DenseMatrix<T>& y,
@@ -23,16 +26,22 @@ template <>
 class naive_sequential_full_SDDMM_HOST<float> : public SDDMMlib<float>
 {
     public:
+        naive_sequential_full_SDDMM_HOST() {}
+        naive_sequential_full_SDDMM_HOST(ExecutionTimer* timer) { this->_timer = timer; }
         virtual void SDDMM(
-            const DenseMatrix<float>& matrixA_HOST,
-            const DenseMatrix<float>& matrixB_HOST,
-            const SparseMatrix<float>& matrixC_HOST,
-            SparseMatrix<float>& matrixResult_HOST) const override;
-
-    private:
-        
+            const DenseMatrix<float>& x,
+            const DenseMatrix<float>& y,
+            const SparseMatrix<float>& z,
+            SparseMatrix<float>& result) const override;
         virtual void start_run() const override;  // Start either cpu or gpu run CHOOSE ONE
         virtual void stop_run() const override;   // Stop either cpu or gpu run CHOOSE ONE
+
+    private:
+        void naive_sequential_full_SDDMM_HOST_CSR(
+            const DenseMatrix<float>& x,
+            const DenseMatrix<float>& y,
+            const CSRMatrix<float>& z,
+            CSRMatrix<float>& result) const;
 };
 
 #endif  // NAIVE_SEQUENTIAL_FULL_SDDMM_HOST_HPP

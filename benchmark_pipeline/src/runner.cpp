000b7982f5decdd8716b521477bbde83fbae2b85
Eric Schreiber <eric.schreiber@gmx.ch>
2023-11-04 13:57:17 +0100

    Extending benchmark pipeline to accept COO and add COO GPU implementation.

--- benchmark_pipeline/src/runner.cpp
+++ benchmark_pipeline/src/runner.cpp
@@ -2,6 +2,7 @@
 
 #include <chrono>
 
+#include "COOMatrix.hpp"
 #include "CSRMatrix.hpp"
 #include "DenseMatrix.hpp"
 #include "config_helper.hpp"
@@ -18,7 +19,7 @@ runner<T>::runner(std::string config_file_path, std::string out_path)
     // Create a results file with results_timestamp.csv
     _results_file_path = _out_path + "/results_" + std::to_string(std::chrono::system_clock::now().time_since_epoch().count()) + ".csv";
     std::ofstream results_file(_results_file_path);
-    results_file << "function,dataset,result" << std::endl;
+    results_file << "function,format,dataset,result" << std::endl;
     results_file.close();
 }
 
@@ -32,41 +33,44 @@ void runner<T>::run()
 {
     init_result_file();
     // Create the matrices
-    CSRMatrix<T> matrixA = CSRMatrix<T>();
+    DenseMatrix<T> matrixA_dense_loader = DenseMatrix<T>();
     DenseMatrix<T> matrixB = DenseMatrix<T>();
     DenseMatrix<T> matrixC = DenseMatrix<T>();
-    CSRMatrix<T> calculatedSolution;
+    // CSRMatrix<T> calculatedSolution;
 
     dataset_paths dataset_before = dataset_paths();
     for (auto& function_to_run : _functions_to_run)
     {
         // Get the function class and dataset
         std::string function_class = std::get<0>(function_to_run);
-        dataset_paths dataset = std::get<1>(function_to_run);
-        SDDMMlib<T>* class_to_run = get_implemented_class<T>(function_class);
+        std::string sparse_matrix_class = std::get<1>(function_to_run);
+        dataset_paths dataset = std::get<2>(function_to_run);
+
         // Check if the dataset has changed
         if (dataset != dataset_before)
         {
-            matrixA.readFromFile(dataset.SparseMatrix_path);
+            matrixA_dense_loader.readFromFile(dataset.SparseMatrix_path);
             matrixB.readFromFile(dataset.DenseMatrixA_path);
             matrixC.readFromFile(dataset.DenseMatrixB_path);
             dataset_before = dataset;
         }
+
+        SDDMMlib<T>* sddmm_to_run = get_implemented_SDDMM<T>(function_class);
+        SparseMatrix<T>* matrixA = get_implemented_SparseMatrix<T>(sparse_matrix_class, matrixA_dense_loader);
+        SparseMatrix<T>* calculatedSolution = get_implemented_SparseMatrix<T>(sparse_matrix_class, matrixA_dense_loader.getNumRows(), matrixC.getNumCols());
+
         // Time the function
         ExecutionTimer timer = ExecutionTimer();
-        class_to_run->set_timer(&timer);
-
-        // Prepare the calculated solution
-        calculatedSolution = CSRMatrix<T>(matrixA.getNumRows(), matrixA.getNumRows());
+        sddmm_to_run->set_timer(&timer);
 
         // Run the function
-        matrixA.SDDMM(
+        matrixA->SDDMM(
             matrixB,
             matrixC,
-            calculatedSolution,
+            *calculatedSolution,
             std::bind(
                 &SDDMMlib<T>::SDDMM,
-                class_to_run,
+                sddmm_to_run,
                 std::placeholders::_1,
                 std::placeholders::_2,
                 std::placeholders::_3,
@@ -74,8 +78,27 @@ void runner<T>::run()
 
         auto durations = timer.get_runtimes();
         // Append the result to the results list
-        _results.push_back(std::make_tuple(function_class, dataset, durations));
+        _results.push_back(std::make_tuple(function_class, sparse_matrix_class, dataset, durations));
         write_result();
+
+        // print the result
+        std::cout << "Result: " << std::endl;
+        for (int i = 0; i < calculatedSolution->getNumRows(); i++)
+        {
+            for (int j = 0; j < calculatedSolution->getNumCols(); j++)
+            {
+                std::cout << calculatedSolution->at(i, j) << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+
+        delete sddmm_to_run;
+        sddmm_to_run = nullptr;
+        delete matrixA;
+        matrixA = nullptr;
+        delete calculatedSolution;
+        calculatedSolution = nullptr;
     }
 }
 
@@ -84,7 +107,7 @@ void runner<T>::init_result_file()
 {
     // Create the results file
     std::ofstream results_file(_results_file_path);
-    results_file << "function,path/to/first/dense/matrix,path/to/second/dense/matrix,path/to/first/sparse/matrix,result" << std::endl;
+    results_file << "function, SparseFormat, path/to/first/dense/matrix, path/to/second/dense/matrix, path/to/first/sparse/matrix, result" << std::endl;
     results_file.close();
 }
 
@@ -95,11 +118,11 @@ void runner<T>::write_result()
     // The structure is string, string, string, string, vector<double>
     auto last_result = _results.back();
     std::string durations_string = "";
-    for (auto& duration : std::get<2>(last_result))
+    for (auto& duration : std::get<3>(last_result))
     {
         durations_string += std::to_string(duration) + ",";
     }
-    dataset_paths dataset = std::get<1>(last_result);
+    dataset_paths dataset = std::get<2>(last_result);
     std::string dataset_string = dataset.DenseMatrixA_path + "," +
                                  dataset.DenseMatrixB_path + "," +
                                  dataset.SparseMatrix_path;

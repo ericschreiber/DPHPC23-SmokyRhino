3d61e6ad534614f01944908041bf451b3c4ebaa1
Eric Schreiber <eric.schreiber@gmx.ch>
2023-11-06 09:22:27 +0100

    Added new not working test.

--- SDDMMlib/tests/SDDMM/test_naive_CPU_SDDMM.cpp
+++ SDDMMlib/tests/SDDMM/test_naive_CPU_SDDMM.cpp
@@ -4,7 +4,7 @@
 #include "DenseMatrix.hpp"
 #include "naive_CPU_SDDMM.hpp"
 
-int main()
+void test_1()
 {
     DenseMatrix<float> matrixA(std::vector<std::vector<float>>{{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}});
     DenseMatrix<float> matrixB(std::vector<std::vector<float>>{{10, 9}, {8, 7}, {6, 5}, {4, 3}, {2, 1}});
@@ -32,16 +32,43 @@ int main()
     class_to_run = nullptr;
 
     assert(calculatedSolution == expectedSolution && "Error: The calculated solution does not match the expected in test_naive_CPU_SDDMM");
+}
 
-    if (calculatedSolution == expectedSolution)
-    {
-        std::cout << "naive CPU SDDMM test passed" << std::endl;
-    }
-    else
-    {
-        std::cout << "Test did not pass" << std::endl;
-    }
-    std::cout << "Test has run" << std::endl;
+void test_2()
+{
+    DenseMatrix<float> matrixA(std::vector<std::vector<float>>{{1, 1, 0, 0, 0}, {0, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}});
+    DenseMatrix<float> matrixB(std::vector<std::vector<float>>{{1, 1, 0, 0, 0}, {0, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}});
+    CSRMatrix<float> matrixS(std::vector<std::vector<float>>{{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}});
+    CSRMatrix<float> calculatedSolution(std::vector<std::vector<float>>{{100, 100, 100, 100, 100}, {100, 100, 100, 100, 100}, {100, 100, 100, 100, 100}, {100, 100, 100, 100, 100}, {100, 100, 100, 100, 100}});
+    CSRMatrix<float> expectedSolution(std::vector<std::vector<float>>{{1, 2, 0, 0, 0}, {0, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}});
+    // This testes the Sparsematrix.SDDMM() (for CSR matrix)
+
+    ExecutionTimer timer = ExecutionTimer();
+    naive_CPU_SDDMM<float>* class_to_run = new naive_CPU_SDDMM<float>(&timer);
+
+    matrixS.SDDMM(
+        matrixA,
+        matrixB,
+        calculatedSolution,
+        std::bind(
+            &naive_CPU_SDDMM<float>::SDDMM,
+            class_to_run,
+            std::placeholders::_1,
+            std::placeholders::_2,
+            std::placeholders::_3,
+            std::placeholders::_4));
+
+    delete class_to_run;
+    class_to_run = nullptr;
+
+    assert(calculatedSolution == expectedSolution && "Error: The calculated solution does not match the expected in test_naive_CPU_SDDMM");
+}
+
+int main()
+{
+    test_1();
+    test_2();
+    std::cout << "Test CPU has run sucessfully" << std::endl;
 
     return 0;
 }

7fdce7d0d566ec80d0a4ff301607068ca397b60f
Niklas Roemer <nroemer@ethz.ch>
2023-11-17 12:41:50 +0100

    test pipeline

--- pipeline_runscripts/run_pipeline.sh
+++ pipeline_runscripts/run_pipeline.sh
@@ -4,9 +4,14 @@
 #SBATCH --error=/dev/null
 #SBATCH --time=00:10:00
 
+
+######
+#       WORK IN PROGRESS
+######
+
 ###
 #   run this script with (replacing the node{ault09/ault10} and using the inputs for the benchmark_run.sh script): 
-#                       sbatch --nodelist=ault09 profiling_pipeline.sh
+#                       sbatch --nodelist=ault09 profiling_launch.sh
 #                       /users/eschreib/niklas/DPHPC23-SmokyRhino/benchmark_pipeline/Pipeline/benchmark_run.sh 
 #                       /users/eschreib/niklas/DPHPC23-SmokyRhino/benchmark_pipeline/tests/config.example.txt 
 #                       /users/eschreib/niklas/DPHPC23-SmokyRhino/profiling/results
@@ -19,14 +24,4 @@ config="$2"
 output_path="$3"
 module load cuda/12.1.1 cmake/3.21.3 gcc/10.2.0
 nvprof --analysis-metrics \
-        --cpu-thread-tracing on \
-        --print-api-trace \
-        --metrics all \
-        --profile-child-processes \
-        --system-profiling on \
-        --trace api,gpu \
-        --track-memory-allocations on \
-        --events all \
-        --csv \
-        --export-profile /users/eschreib/niklas/DPHPC23-SmokyRhino/profiling/results/output%p.nvprof \
-        $script $config $output_path > /users/eschreib/niklas/DPHPC23-SmokyRhino/profiling/results/profiling.out 2>&1
+$script $config $output_path
--- profiling/profiling_single_executable.sh
+++ profiling/profiling_single_executable.sh
@@ -6,7 +6,7 @@
 
 ###
 #   run this script with (replacing the node{ault09/ault10} and the (test)executable with your own): 
-#                       sbatch --nodelist=ault09 profiling_launch.sh \
+#                       sbatch --nodelist=ault09 profiling_single_executable.sh \
 #                       ./build/SDDMMlib/tests/SDDMM/naive_dense_dense_gpu/test_compile 
 #
 #   this script runs for max 10 minutes, so if your executable takes longer, you need to adjust the time

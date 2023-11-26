27de13fdbd055091cd26cf0d37f371a137cb6ae1
marcusmoller01 <marcus.moller.swed@gmail.com>
2023-11-26 18:47:39 +0100

    Added sparse matrix indexing

--- SDDMMlib/src/SDDMM/coo_SM_SM_SDDMM_gpu/coo_SM_SM_SDDMM.cu
+++ SDDMMlib/src/SDDMM/coo_SM_SM_SDDMM_gpu/coo_SM_SM_SDDMM.cu
@@ -6,58 +6,124 @@
 #include "naive_coo_gpu/naive_coo_SDMM.cuh"
 #include "utils.h"
 
-__device__ float dot_product(
-    const int k,
-    const float* __restrict__ const matrixA_GPU_values_row,
-    const float* __restrict__ const matrixB_transposed_GPU_values_col)
-{
-    // calculate SUM_i row[i] * col[i]
-    float result = 0;
-    for (int i = 0; i < k; i++)
-    {
-        result += matrixA_GPU_values_row[i] * matrixB_transposed_GPU_values_col[i];
-    }
-    return result;
-}
+#define THREADS_PER_BLOCK 2
+#define SHARED_MEM_SIZE_BYTES 8  // this is the size of shared mem on both the A100 and V100 GPUs.
+                                 // can force tiling (e.g. for testing) by setting this to something small.
+#define SHARED_MEM_SIZE SHARED_MEM_SIZE_BYTES / sizeof(float)
 
-__device__ float naive_coo_one_val(
+__global__ void SM_SM_coo(
+    const int T_ij,
+    const int blocks,
+    const int last_T_i,
+    const int tiling_steps,
+    const int last_T_j,
     const int k,
-    const float multiplier,
-    const float* __restrict__ const matrixA_GPU_values_row,
-    const float* __restrict__ const matrixB_transposed_GPU_values_col)
-{
-    // calculate mutiplier * SUM_i row[i] * col[i]
-    return multiplier * dot_product(k, matrixA_GPU_values_row, matrixB_transposed_GPU_values_col);
-}
-
-// Assumes matrixB_transposed_GPU_values is transposed
-__global__ void naive_coo(
-    const int k,
-    const int numElementsC,
     const float* __restrict__ const matrixA_GPU_values,
     const float* __restrict__ const matrixB_transposed_GPU_values,
     const float* __restrict__ const matrixC_GPU_values,
     const int* __restrict__ const matrixC_GPU_row_indices,
     const int* __restrict__ const matrixC_GPU_col_indices,
-    float* __restrict__ const matrixResult_GPU_values)
+    float* __restrict__ const matrixResult_GPU_values,
+    const int* nnz_indexing_array)
 {
-    int index = (blockIdx.x * blockDim.x) + threadIdx.x;
-
-    if (index < numElementsC)
+    // Define necessary parameters
+    int block_index = blockIdx.x;
+    // Set T_i for the block
+    int T_i = T_ij;
+    // This presumes that blocks are indexed at 0
+    if (block_index == blocks - 1)
     {
-        int row = matrixC_GPU_row_indices[index];
-        int col = matrixC_GPU_col_indices[index];
-        float multiplier = matrixC_GPU_values[index];
+        T_i = last_T_i;
+    }
 
-        // calculate matrixResult_GPU_values[index][col] = naive_coo_one_val(multiplier, matrixA_GPU_values[row][:], matrixB_GPU_values[:][col])
-        matrixResult_GPU_values[index] = naive_coo_one_val(k, multiplier, matrixA_GPU_values + (row * k), matrixB_transposed_GPU_values + (col * k));
+    // Declare pointer to start of A for the block
+    const float* A_vals_row_start = matrixA_GPU_values + (block_index * T_ij * k);
+
+    // Save A in Shared Memory for full reuse within a block
+    extern __shared__ float A_in_row_panel[];
+    if (threadIdx.x == 0)
+    {
+        // Iterate over the rows in block
+        for (int i = 0; i < T_i * k; i++)
+        {
+            A_in_row_panel[i] = *(A_vals_row_start + i);
+        }
+    }
+
+    // Main loop over the tiles in a row panel
+    for (int tile = 0; tile < tiling_steps; tile++)
+    {
+        // For each tile declare pointer to beginning of B transposed
+        const float* B_vals_row_start = matrixB_transposed_GPU_values + (tiling_steps * T_ij * k);
+        // Set T_j for tile
+        int T_j = T_ij;
+        if (tile == tiling_steps - 1)
+        {
+            T_j = last_T_j;
+        }
+
+        // Save B in Shared Memory for reuse within a tile
+        extern __shared__ float B_in_tile[];
+        if (threadIdx.x == 0)
+        {
+            // Iterate over B transpose rows in a tile
+            for (int i = 0; i < T_j * k; i++)
+            {
+                B_in_tile[i] = *(B_vals_row_start + i);
+            }
+        }
+        __syncthreads();
+
+        // Smart way of dividing work between all the threads:
+        int c_index;
+        for (int i = 0; i < T_i; i++)
+        {
+            c_index = nnz_indexing_array[(block_index * tiling_steps + tile) * T_ij + i];
+            printf("block: %d, tile[%d], partial row: %d, nnz's: %d\n", block_index, tile, tile, c_index);
+        }
     }
 }
 
-void compute_naive_coo(
+__global__ void precomputation(
+    int* nnz_indexing_array,
+    const int* __restrict__ const matrixC_GPU_row_indices,
+    const int* __restrict__ const matrixC_GPU_col_indices,
+    int tiling_steps,
+    int numBlocks,
+    int T_ij,
+    const int numElementsC,
+    const int m)
+{
+    // In this function is the sparse matrix indexed for tiling
+    // Loop over the blocks
+    int row;
+    int col;
+    int block;
+    int tile;
+    int row_in_block;
+
+    // This loop counts the nbr of nnz in a given row in a tile
+    for (int i = 0; i < numElementsC; i++)
+    {
+        row = matrixC_GPU_col_indices[i];
+        col = matrixC_GPU_col_indices[i];
+        block = row / numBlocks;
+        tile = col / tiling_steps;
+        row_in_block = row - block * T_ij;
+        nnz_indexing_array[(block * tiling_steps + tile) * T_ij + row_in_block] += 1;
+    }
+
+    // m*tiling_steps is the len of nnz_indexing_array, this loop makes the count cummulative
+    for (int i = 1; i < m * tiling_steps; i++)
+    {
+        nnz_indexing_array[i] += nnz_indexing_array[i - 1];
+    }
+}
+
+void compute(
     const int m,
     const int n,
-    const int k,
+    const int k,  // number of rows of B
     const int numElementsC,
     const float* __restrict__ const matrixA_GPU_values,
     const float* __restrict__ const matrixB_transposed_GPU_values,
@@ -66,29 +132,44 @@ void compute_naive_coo(
     const int* __restrict__ const matrixC_GPU_col_indices,
     float* __restrict__ const matrixResult_GPU_values)
 {
-    // Iterate of matrixC_GPU_row_indices, matrixC_GPU_col_indices, matrixC_GPU_values (as they share indices)
-    // Each thread will calculate one value of the result matrix
+    // MY CODE
+    // Tiling with T_i = T_j = T_ij based on size of shared memory
+    int T_ij = (float)(SHARED_MEM_SIZE_BYTES) / (2 * k);
+    // Each block calculates one row panel (and each tile in sequence)
+    int blocks = ceil(m / T_ij);
+    // The last block may get fewer rows if m isnt dividable by T_ij
+    int last_T_i = m % T_ij;
+    // Each block computes the tiles in a row-panel -> row_len/tile_width
+    int tiling_steps = ceil(n / T_ij);
+    // Last tile may get fewer columns from B if n isnt divicable by T_ij
+    int last_T_j = n % T_ij;
+    // This array gets filled up nnz index per row in each tile and block
+    int* nnz_indexing_array;
+    CUDA_CHECK(cudaMalloc((void**)&nnz_indexing_array, (m * tiling_steps) * sizeof(int)));
 
-    dim3 threadsPerBlock(1024);
-    int blocks = (numElementsC + threadsPerBlock.x - 1) / threadsPerBlock.x;
+    precomputation<<<1, 1>>>(nnz_indexing_array, matrixC_GPU_row_indices, matrixC_GPU_col_indices, tiling_steps, blocks, T_ij, numElementsC, m);
 
-    // std::cout << "n: " << n << std::endl;
-    // std::cout << "m: " << m << std::endl;
-    // std::cout << "k: " << k << std::endl;
-    // std::cout << "numElementsC: " << numElementsC << std::endl;
-    // std::cout << "blocks: " << blocks << std::endl;
-    // std::cout << "threadsPerBlock: " << threadsPerBlock.x << std::endl;
+    dim3 threadsPerBlock(THREADS_PER_BLOCK);
 
-    naive_coo<<<blocks, threadsPerBlock>>>(
+    // Call main kernel
+    SM_SM_coo<<<blocks, threadsPerBlock, SHARED_MEM_SIZE>>>(
+        T_ij,
+        blocks,
+        last_T_i,
+        tiling_steps,
+        last_T_j,
         k,
-        numElementsC,
         matrixA_GPU_values,
         matrixB_transposed_GPU_values,
         matrixC_GPU_values,
         matrixC_GPU_row_indices,
         matrixC_GPU_col_indices,
-        matrixResult_GPU_values);
+        matrixResult_GPU_values,
+        nnz_indexing_array);
     // Aggregate the return value of the kernel
     CUDA_CHECK(cudaGetLastError());
     CUDA_CHECK(cudaDeviceSynchronize());
+
+    // free the array prevBlocksWork on GPU
+    CUDA_CHECK(cudaFree(nnz_indexing_array));
 }

aa4577df2c27bb477094040e885c2294206d5812
Eric Schreiber <eric.schreiber@gmx.ch>
2024-01-03 13:36:19 +0100

    Naive COO updated to conform to new SDDMM standard.

--- SDDMMlib/src/SDDMM/naive_coo_gpu/naive_coo_SDDMM.cu
+++ SDDMMlib/src/SDDMM/naive_coo_gpu/naive_coo_SDDMM.cu
@@ -20,23 +20,12 @@ __device__ float dot_product(
     return result;
 }
 
-__device__ float naive_coo_one_val(
-    const int k,
-    const float multiplier,
-    const float* __restrict__ const matrixA_GPU_values_row,
-    const float* __restrict__ const matrixB_transposed_GPU_values_col)
-{
-    // calculate mutiplier * SUM_i row[i] * col[i]
-    return multiplier * dot_product(k, matrixA_GPU_values_row, matrixB_transposed_GPU_values_col);
-}
-
 // Assumes matrixB_transposed_GPU_values is transposed
 __global__ void naive_coo(
     const int k,
     const int numElementsC,
     const float* __restrict__ const matrixA_GPU_values,
     const float* __restrict__ const matrixB_transposed_GPU_values,
-    const float* __restrict__ const matrixC_GPU_values,
     const int* __restrict__ const matrixC_GPU_row_indices,
     const int* __restrict__ const matrixC_GPU_col_indices,
     float* __restrict__ const matrixResult_GPU_values)
@@ -47,10 +36,9 @@ __global__ void naive_coo(
     {
         int row = matrixC_GPU_row_indices[index];
         int col = matrixC_GPU_col_indices[index];
-        float multiplier = matrixC_GPU_values[index];
 
         // calculate matrixResult_GPU_values[index][col] = naive_coo_one_val(multiplier, matrixA_GPU_values[row][:], matrixB_GPU_values[:][col])
-        matrixResult_GPU_values[index] = naive_coo_one_val(k, multiplier, matrixA_GPU_values + (row * k), matrixB_transposed_GPU_values + (col * k));
+        matrixResult_GPU_values[index] = dot_product(k, matrixA_GPU_values + (row * k), matrixB_transposed_GPU_values + (col * k));
     }
 }
 
@@ -61,7 +49,6 @@ void compute_naive_coo(
     const int numElementsC,
     const float* __restrict__ const matrixA_GPU_values,
     const float* __restrict__ const matrixB_transposed_GPU_values,
-    const float* __restrict__ const matrixC_GPU_values,
     const int* __restrict__ const matrixC_GPU_row_indices,
     const int* __restrict__ const matrixC_GPU_col_indices,
     float* __restrict__ const matrixResult_GPU_values)
@@ -84,7 +71,6 @@ void compute_naive_coo(
         numElementsC,
         matrixA_GPU_values,
         matrixB_transposed_GPU_values,
-        matrixC_GPU_values,
         matrixC_GPU_row_indices,
         matrixC_GPU_col_indices,
         matrixResult_GPU_values);
